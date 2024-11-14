5367da6f0e5aef74e9636dd35ac93637238008c1
ZinirahUwitonze <zinira1rachid@gmail.com>
2024-11-14 17:38:23 +0200

    Added new photos and file

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Photography Gear Store ready to serve!");
+  // Add any interactive features here, like product filtering or toggling details.
+});
